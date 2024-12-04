96824f5753113ca7358cb16f81448641d6d35e79
miti <muntajimamahbub5@gmail.com>
2024-12-05 02:58:40 +0600

    Data stored in the server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,15 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// crowdFunding
-// WeK5HZZWl019Yd31
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
 
-
-
-const uri = "mongodb+srv://crowdFunding:WeK5HZZWl019Yd31@cluster0.oi99s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-//const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.oi99s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = `mongodb+srv://${user}:${password}@cluster0.oi99s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,13 +27,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
+
+    const campaignCollection = client.db('campaignDB').collection('campaign');
+
+    app.get('/campaign', async(req, res) =>{
+      const cursor = campaignCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/campaign', async(req, res) =>{
+      const newCampaign = req.body;
+      console.log(newCampaign);
+      const result = await campaignCollection.insertOne(newCampaign);
+      res.send(result);
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
