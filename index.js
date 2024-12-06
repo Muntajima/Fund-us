7d6c5d0f2933ac93ee1a8693199d75515c11b0a4
miti <muntajimamahbub5@gmail.com>
2024-12-06 11:19:14 +0600

    User can delete campaign from db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -61,7 +61,12 @@ async function run() {
       res.send(result);
     })
 
-
+    app.delete('/campaign/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await campaignCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
